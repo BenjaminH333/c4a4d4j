019390b2fe4d09f4b467ce0a4362280dbea14d30
phantamanta44 <evandalong@gmail.com>
2016-11-04 22:30:22 -0500

    Simple permission prerequisite

--- src/main/java/io/github/phantamanta44/c4a4d4j/PrereqParser.java
+++ src/main/java/io/github/phantamanta44/c4a4d4j/PrereqParser.java
@@ -1,17 +1,40 @@
 package io.github.phantamanta44.c4a4d4j;
 
 import io.github.phantamanta44.commands4a.command.Prerequisite;
+import sx.blah.discord.handle.obj.Permissions;
+
+import java.util.function.Predicate;
 
 public class PrereqParser {
 
     private final String pStr;
+    private Prerequisite<CmdCtx> prereq;
 
     public PrereqParser(String prereq) {
         this.pStr = prereq;
     }
 
     public Prerequisite<CmdCtx> getPrereq() {
-        return new Prerequisite<>(ctx -> true, "Impossible condition!"); // TODO Implement
+        if (prereq != null)
+            resolvePrereq();
+        return prereq;
+    }
+
+    private void resolvePrereq() {
+        if (!pStr.contains(":"))
+            throw new IllegalArgumentException("Illegal prerequisite format!");
+        String type = pStr.substring(0, pStr.indexOf(":")), spec = pStr.substring(pStr.indexOf(":") + 1);
+        switch (type) {
+            case "perm":
+                try {
+                    Permissions perm = Permissions.valueOf(spec.toUpperCase());
+                    Predicate<CmdCtx> test = c -> c.getChannel().getModifiedPermissions(c.getAuthor()).contains(perm);
+                    prereq = new Prerequisite<>(test, String.format("Requires permission %s!", perm.name()));
+                } catch (IllegalArgumentException e) {
+                    throw new IllegalArgumentException("No such permission: " + spec);
+                }
+                break;
+        }
     }
 
 }
